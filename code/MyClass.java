fc8f5d29f7411d573c76ef9b34af6707beed9b60
lukaneco <luca.sain@outlook.com>
2020-03-23 14:27:37 -0300

    gg

---
+++ code/MyClass.java
@@ -0,0 +1,15 @@
+
+// Create a MyClass class
+public class MyClass {
+  int x;
+
+  // Create a class constructor for the MyClass class
+  public MyClass() {
+    x = 5;
+  }
+
+  public static void main(String[] args) {
+    MyClass myObj = new MyClass();
+    System.out.println(myObj.x);
+  }
+}
